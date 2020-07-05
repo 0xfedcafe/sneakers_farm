6035ca9defcfc85455613368740d05c369e737c9
Ihor Roik <ihorroik@tests-MacBook-Pro.local>
2020-07-05 14:23:12 +0300

    gitignore

--- config/sequelize.js
+++ config/sequelize.js
@@ -3,7 +3,7 @@ import config from './keys'
 
 class Seq{
   constructor(){
-    return new Sequelize(config.sql.database,config.sql.user,null,{
+    return new Sequelize(config.sql.database,config.sql.user,config.sql.password,{
       host:config.sql.host,
       dialect:'mysql',
       logging: false
